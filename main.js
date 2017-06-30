120a09094b8155689a6604bae350600350bfa9c7
Chris Voss <chrisvoss92@gmail.com>
2017-06-30 10:39:21 -0700

    User can see the car.

---
+++ main.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+
+class Car {
+  constructor(direction, speed, location, $car) {
+    this.direction = direction
+    this.speed = speed
+    this.location = location
+    this.$car = $car
+  }
+  static start(car) {
+    setInterval(() => car.move(), 16)
+  }
+  turn(newDirection) {
+    this.direction = newDirection
+  }
+  accelerate(amount) {
+    this.speed += amount
+  }
+  move() {
+    switch (this.direction) {
+      case 'north' : this.location[1] -= this.speed
+        break
+      case 'west' : this.location[0] -= this.speed
+        break
+      case 'south' : this.location[1] += this.speed
+        break
+      case 'east' : this.location[0] += this.speed
+        break
+    }
+  }
+  static clone(car) {
+    return new this(car.direction, car.speed, car.location.slice())
+  }
+}
+
+const f1Car = new Car('east', 0, [0, 0], document.getElementById('car'))
